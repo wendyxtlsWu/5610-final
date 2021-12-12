a716d848e08c4de501cc3a0a7542ab1b89a181ec
Xiaowei <67398577+Xiaowei-Lu@users.noreply.github.com>
2021-12-12 01:00:47 -0800

    Add files via upload

---
+++ clicked/clicked.js
@@ -0,0 +1,77 @@
+import React from "react";
+import {BrowserRouter, Route} from "react-router-dom";
+import NavbarComponent from "../navbar/navbarComponent";
+import PetDetailComponent from "../petDetail/petDetailComponent";
+import LoginComponent from "../loginAndRegister/loginComponent";
+import RegisterComponent from "../loginAndRegister/registerComponent";
+import homePage from "../home/homePage";
+import ProfilePageComponent from "../users/ProfilePageComponent";
+
+class Clicked extends React.Component {
+    state = {
+        currentUser: {
+            userId: '',
+            username: '',
+            email: '',
+            zipCode: '',
+            userType: ''
+        }
+    }
+
+    // handleLogout = () => {
+    //     logout().then(() =>
+    //         this.setCurrentUser({
+    //             userId: '',
+    //             username: '',
+    //             email: '',
+    //             zipCode: '',
+    //             userType: ''
+    //         })
+    //     )
+    // }
+
+    setCurrentUser = (user) => {
+        this.setState({
+            currentUser: user
+        })
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="container-fluid" style={{width: '100%', height: '100%'}}>
+                    <Route path="/">
+                         <NavbarComponent/>
+                    </Route>
+                    <Route path="/" exact={true} render={(props) =>
+                        <homePage
+                            {...props}
+                            currentUser={this.state.currentUser}/>}/>
+                    {/*// pass the props and the setCurrentUser function to components*/}
+                    <Route path={"/login"} exact={true} render={(props) =>
+                        <LoginComponent {...props} setCurrentUser = {this.setCurrentUser}/>}/>
+
+                    <Route path="/register" exact={true} render={(props) =>
+                        <RegisterComponent
+                            {...props}
+                            setCurrentUser={this.setCurrentUser}/>}/>
+
+                    <Route path="/profile" exact={true} render={(props) =>
+                        <ProfilePageComponent
+                            {...props}
+                            userId={this.state.currentUser.id}
+                            setCurrentUser={this.setCurrentUser}/>}/>
+
+                    <Route path="/details" exact={true}>
+                        <PetDetailComponent/>
+                    </Route>
+
+                </div>
+            </BrowserRouter>
+
+        )
+    }
+
+}
+
+export default Clicked
