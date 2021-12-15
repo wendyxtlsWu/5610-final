16ef50c4476965034b818be2d30517fa51052114
wujingjing <wendyxtls@gmail.com>
2021-12-14 16:57:57 -0800

    fix bug

--- src/component/navbar/navbarComponent.js
+++ src/component/navbar/navbarComponent.js
@@ -19,37 +19,34 @@ class NavbarComponent extends React.Component {
                                 <a className="btn btn-success mx-1" href="/login">Log in <i className="fa fa-user"/></a>
                                 <a className="btn btn-primary mx-1" href="/register">Signup <i
                                     className="fa fa-user-plus"/></a>
-
                             </span>
-
                         }
-                        <div className="row">
+
                         {
                             this.props.currentUser.username !== '' &&
-                                <>
-                                    <span>
-                                        <a className="btn btn-secondary mx-1" href="/search">Search <i className="fas fa-search fa-lg"></i></a>
+                            <div className="row" id="container">
+
+                                    <a className="btn btn-secondary mx-1" href="/search">Search <i className="fas fa-search fa-lg"></i></a>
+
+                                <div className="dropdown show">
+                                    <button className="btn btn-secondary dropdown-toggle ml-1" type="button"
+                                            id="dropdownMenuButton"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <span className="">
+                                        Hello {this.props.currentUser.username}
+                                        <i className="fa fa-user ml-3"/>
+
                                     </span>
-                                    <div className="dropdown show">
-                                        <button className="btn btn-secondary dropdown-toggle ml-1" type="button"
-                                                id="dropdownMenuButton"
-                                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <span className="">
-
-                                            Hello {this.props.currentUser.username}
-                                            <i className="fa fa-user ml-3"/>
-
-                                        </span>
-                                        </button>
-                                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                            <a className="dropdown-item" href="/profile">Profile</a>
-                                            <div className="dropdown-divider"></div>
-                                            <a className="dropdown-item" href="/" onClick={this.props.handleLogout}>Log out</a>
-                                        </div>
+                                    </button>
+                                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <a className="dropdown-item" href="/profile">Profile</a>
+                                        <div className="dropdown-divider"></div>
+                                        <a className="dropdown-item" href="/" onClick={this.props.handleLogout}>Log out</a>
                                     </div>
-                                </>
+                                </div>
+
+                            </div>
                         }
-                        </div>
                     </div>
                 </div>
             </nav>
